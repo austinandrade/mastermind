4097e5abd3ce5f2cf6a4c4af39480e37e85672e2
JoannaCoPo <jococopo@gmail.com>
2021-02-15 18:13:38 -0700

    Add time elapsed function

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,26 +1,35 @@
+
 require 'pry'
+require 'time'
 require './lib/printer'
 require './lib/player_guess_matcher'
+
 class Mastermind
-  attr_reader :winning_code, :instance_of_matcher
+  attr_reader :winning_code
   attr_accessor :guess_count, :new_guess_count
+
   def initialize (guess_count = nil, winning_code = nil)
-    @guess_count = 0 #guess_count.zero? ? increment_guess_count : guess_count
+    @guess_count = 0
     @winning_code = create_winning_code
-    #ternary - if/els
-    # @player_guess = player_guess
     @instance_of_printer = Printer.new(@winning_code, @guess_count, @player_guess)
-    @game_over = false
-    # binding.pry
     @instance_of_matcher = PlayerGuessMatcher.new(@winning_code, @player_guess)
+    @game_over = false
+    # @elapsed_total = elapsed_total
+    @start_time = nil
+    @end_time = nil
+    # @time_state = []
+    # @elapsed = @time_state[1] - @time_state[0]
+
   end
+
   def welcome_to_mastermind
     @instance_of_printer.welcome_message
   end
+
   def beginning_user_input
     player_input = gets.chomp
     if player_input == "p" || player_input == "play"
-        start_game.start_time
+       start_game
     elsif player_input == "i" || player_input == "instructions"
         @instance_of_printer.instructions_message
     else player_input == "q" || player_input == "quit"
@@ -28,8 +37,9 @@ class Mastermind
         exit
     end
   end
+
   def game_logic(player_guess)
-    until @game_over == true
+    while @game_over == false
     # until @guess_count == 1000000
       if @player_guess == "c" || @player_guess == "cheat"
         @instance_of_printer.cheat_message
@@ -48,21 +58,21 @@ class Mastermind
         @instance_of_matcher.check_number_of_correct_colors(@player_guess)
         @instance_of_matcher.check_number_of_correct_color_and_position(@player_guess)
         @instance_of_printer.guess_feedback(@player_guess, @guess_count)
-      elsif @player_guess == @winning_code
+      else @player_guess == @winning_code
+        set_end_time
+        # time_state(start_time, end_time)
+        time_message = time_elapsed 
         @game_over = true
-        @instance_of_printer.win_message(guess_count)
-          play_or_quit_input = gets.chomp
-          post_win(play_or_quit_input)
-          break
-        # guess_feedback = "'#{player_guess}' has 3 of the correct elements with 2 in the correct positions
-        # You've taken #{@guess_count} guess"
-        # puts guess_feedback
-        # @guess_count += 1
-        # Mastermind.new(@guess_count, @winning_code).start_game
+        @instance_of_printer.win_message(@guess_count, time_message)
+        # binding.pry
+        play_or_quit_input = gets.chomp
+        post_win(play_or_quit_input)
+        break
       end
       @player_guess = gets.chomp
     end
   end
+
   def post_win(play_or_quit_input)
     if play_or_quit_input ==  "p" || play_or_quit_input == "play"
       Mastermind.new.start_game
@@ -71,13 +81,14 @@ class Mastermind
       exit
     end
   end
+
   def start_game
     @instance_of_printer.start_game_message
     @player_guess = gets.chomp
-    # guess_count = 1
-    # guess_count += 1
+    set_start_time
     game_logic(@player_guess)
   end
+
   def create_winning_code
     colors = ['r', 'r', 'g', 'g', 'b', 'b', 'y', 'y']
     shuffled_colors = colors.shuffle
@@ -91,29 +102,31 @@ class Mastermind
     @winning_code = color_collector.join("")
   end
 
-  def start_time
-  start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-  starty_time = []
-  stary_time << start_time
-  starty_time
+  def set_start_time
+   @start_time = Time.now
+  end
+  #
+  def set_end_time
+    @end_time = Time.now
   end
 
-  def end_time
-  end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-  endy_time = []
-  endy_time << end_time
-  endy_time
-  end
+  # def time_state(start_time, end_time)
+  #     time_state = []
+  #     time_state << start_time
+  #     time_state << end_time
+  #     binding.pry
+  #     time_state
+  # end
 
-  def total_time
-  unrounded_time = end_time[0] - start_time[0]
-  seconds = total_time % 60
-  minutes = (seconds / 60) % 60
-  seconds_rounded = seconds.round(1)
-  minutes_rounded = minutes.floor(1)
-  unrounded_time
-  end
+  def time_elapsed
+    elapsed = @end_time - @start_time
+    total = elapsed / 60
+    minutes = total.to_i
+    seconds = ((total.round(2) - minutes) * 100).to_i
+    # binding.pry
 
+    "#{minutes} minutes and #{seconds} seconds"
+  end
 
 end
 
--- lib/printer.rb
+++ lib/printer.rb
@@ -1,39 +1,62 @@
 require './lib/player_guess_matcher'
+# require './lib/time'
+
 class Printer
-  attr_reader :winning_code, :time_taken_to_win
+  attr_reader :winning_code, :time_taken_to_win, :elapsed_total
   attr_accessor :guess_count, :player_guess
-  def initialize(winning_code, guess_count, player_guess) # change nil if need be
+
+  def initialize(winning_code, guess_count, player_guess) #time_taken_to_win) # change nil if need be
     @winning_code = winning_code
     @guess_count = guess_count
     @player_guess = player_guess
-    # @start_time = start_time
-    # @end_time = end_time
-    # @total_time = total_time
     # @time_taken_to_win = time_taken_to_win
     @instance_of_matcher = PlayerGuessMatcher.new(@winning_code, @player_guess)
+    # @instance_of_time = Time.new(@starting_time, @end_time)
+    # @instance_of_mastermind = Mastermind.new(@guess_count, @winning_code)
+    @elapsed_total = elapsed_total
+
   end
+
   def welcome_message
     welcome_message = "Welcome to MASTERMIND
     Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+
     puts welcome_message
   end
+
   def handle_guess_singular_or_plural(guess_count)
     @guesses_or_guess = guess_count > 1 ? "guesses" : "guess"
   end
+
+  # def handle_minute_singular_or_plural(end_time_minutes, start_time_minutes)
+  #   @minutes_or_minute = end_time_minutes - start_time_minutes = 1 ? "minute" : "minutes" #instance variables?
+  # end
+
   def guess_feedback(player_guess, guess_count) #had to add ability to pass in argument into method - because player_guess is user input??
     puts "------------------"
+
     handle_guess_singular_or_plural(guess_count)
+
     guess_feedback = "'#{player_guess}' has #{@instance_of_matcher.check_number_of_correct_colors(player_guess)} of the correct elements with #{@instance_of_matcher.check_number_of_correct_color_and_position(player_guess)} in the correct positions
     You've taken #{guess_count} #{@guesses_or_guess}"
+
     puts guess_feedback
   end
-  def win_message(guess_count)
+
+  def win_message(guess_count, time_message)
     puts "------------------"
+
     handle_guess_singular_or_plural(guess_count)
-    win_message = "Congratulations! You guessed the sequence '#{@winning_code}' in #{@guess_count} #{@guesses_or_guess} over #{} seconds!
+    # handle_minute_singular_or_plural(end_time_minutes, start_time_minutes)
+    # minutes_elapsed = end_time_minutes - start_time_minutes
+    # seconds_elapsed = end_time_seconds - start_time_seconds
+    # binding.pry
+    win_message = "Congratulations! You guessed the sequence '#{@winning_code}' in #{guess_count} #{@guesses_or_guess} over #{time_message}!
     Do you want to (p)lay again or (q)uit?"
+
     puts win_message
   end
+
   def start_game_message
     puts "------------------"
     start_game_message = "I have generated a beginner sequence with four elements made up of:
@@ -42,33 +65,37 @@ Use (q)uit at any time to end the game.
 What's your guess?"
     puts start_game_message
     puts "------------------"
+
   end
+
   def instructions_message
     puts "------------------"
     instructions = "You must successfully guess the secret code. On each guess, you will learn the number of correct colors guessed as well as the number placed in the correct slots"
+
     puts instructions
     puts "------------------"
   end
+
   def quit_message
     puts "------------------"
     p "Exiting..."
     puts "------------------"
   end
+
   def answer_is_too_long_message
     puts "------------------"
     puts "ANSWER IS TOO LONG!"
     puts "------------------"
-    puts "Try again..."
   end
+
   def answer_is_too_short_message
     puts "------------------"
     puts "ANSWER IS TOO SHORT!"
     puts "------------------"
-    puts "Try again..."
   end
+
   def cheat_message
     puts "------------------"
     puts "Winning code: #{@winning_code}"
-    puts "Nice. What's your guess cheater?"
   end
 end
